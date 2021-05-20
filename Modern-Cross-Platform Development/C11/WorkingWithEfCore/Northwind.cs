e06cf43706741b0bd3a47410ef849974f74b0f53
Serge <nserge05@mail.com>
2021-05-20 18:20:43 +0200

    product class

--- Modern-Cross-Platform Development/C11/WorkingWithEfCore/Northwind.cs
+++ Modern-Cross-Platform Development/C11/WorkingWithEfCore/Northwind.cs
@@ -1,7 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Packt.Shared
 {
-    public class Northwind
+    public class Northwind : DbContext
     {
+        public DbSet<Catergory> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "Northwind.db");
 
+            optionsBuilder.UseSqlite($"Filename={path}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+             modelBuilder.Entity<Catergory>()
+             .Property(catergory => catergory.CatergoryName)
+             .IsRequired()
+             .HasMaxLength(15);
+
+             modelBuilder.Entity<Product>()
+             .Property(product => product.Cost)
+             .HasConversion<double>();
+        }
     }
 }
--- Modern-Cross-Platform Development/C11/WorkingWithEfCore/Product.cs
+++ Modern-Cross-Platform Development/C11/WorkingWithEfCore/Product.cs
@@ -1,7 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Packt.Shared
 {
     public class Product
     {
-
+        public int ProductID {get; set;}
+        [Required]
+        [StringLength(40)]
+        public string ProductName {get ; set;}
+        [Column("UnitPrice",TypeName="money")]
+        public decimal? Cost {get; set;} // property name != field name
+        [Column("UnitsInStock")]
+        public short? Stock {get; set;}
+        public bool Discontinued {get; set;}
+        public int CategoryID {get; set;}
+        public virtual Catergory Catergory {get; set;}
     }
 }
