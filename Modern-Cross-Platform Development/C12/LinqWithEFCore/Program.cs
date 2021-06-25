b44b700fcef4db3a81fd4f643d66e9dba77758ad
Serge-N <nserge05@gmail.com>
2021-06-25 13:03:21 +0200

    filtering and sorting linq

---
+++ Modern-Cross-Platform Development/C12/LinqWithEFCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace LinqWithEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilterAndSort();
+        }
+
+
+        static void FilterAndSort()
+        {
+            using (var db = new Northwind())
+            {
+                var query = db.Products
+                .Where(product => product.UnitPrice < 10M)
+                .OrderByDescending(product => product.UnitPrice);
+
+                WriteLine("Products that cost less than $10:");
+
+                foreach (var item in query)
+                {
+                    WriteLine("{0}: {1} costs {2:$#,##0.00}",
+                    item.ProductID, item.ProductName, item.UnitPrice);
+                }
+
+                WriteLine();
+            }
+        }
+    }
+}
