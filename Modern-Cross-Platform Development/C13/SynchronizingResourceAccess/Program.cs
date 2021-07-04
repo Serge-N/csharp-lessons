3784c4be31c733a63bc4f10b361a33e0cbf15f0d
Serge-N <nserge05@gmail.com>
2021-07-04 14:29:07 +0200

    Thread Locks

--- Modern-Cross-Platform Development/C13/SynchronizingResourceAccess/Program.cs
+++ Modern-Cross-Platform Development/C13/SynchronizingResourceAccess/Program.cs
@@ -9,9 +9,12 @@ namespace SynchronizingResourceAccess
     class Program
     {
         static Random r = new Random();
+        static object conch = new Object();
+        static int Counter;
         static string Message; // a shared resource
         static void Main(string[] args)
         {
+
             WriteLine("Please wait for the tasks to complete.");
             Stopwatch watch = Stopwatch.StartNew();
 
@@ -20,28 +23,56 @@ namespace SynchronizingResourceAccess
 
             Task.WaitAll(new Task[] { a, b });
 
+            // watch out for Deadlocks - use the monitor in a try statement to avoid it.
+
             WriteLine();
             WriteLine($"Results: {Message}.");
-            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+            string value = $"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.";
+            WriteLine(value);
+
+            WriteLine($"{Counter} string modifications.");
+
+            // avoid using events in multithreaded scenarios
+            // Link: https://docs.microsoft.com/en-us/archive/blogs/cburrows/field-like-events-considered-harmful
         }
 
         static void MethodA()
         {
-            for (int i = 0; i < 5; i++)
+            try
             {
-                Thread.Sleep(r.Next(2000));
-                Message += "A";
-                Write(".");
+                if (Monitor.TryEnter(conch, TimeSpan.FromSeconds(15)))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Thread.Sleep(r.Next(2000));
+                        Message += "A";
+                        Interlocked.Increment(ref Counter);
+                        Write(".");
+                    }
+                }
+                else
+                {
+                    WriteLine("Method A failed to enter a monitor lock.");
+                }
             }
+            finally
+            {
+                Monitor.Exit(conch);
+            }
+
         }
 
         static void MethodB()
         {
-            for (int i = 0; i < 5; i++)
+            lock (conch)
             {
-                Thread.Sleep(r.Next(2000));
-                Message += "B";
-                Write(".");
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Interlocked.Increment(ref Counter);
+                    Write(".");
+                }
             }
         }
     }
