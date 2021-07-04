8bc1a6b9dcf3f0d89f9a1640c5f57201829a1040
Serge-N <nserge05@gmail.com>
2021-07-04 14:48:22 +0200

    async and await

--- Modern-Cross-Platform Development/C13/AsyncConsole/Program.cs
+++ Modern-Cross-Platform Development/C13/AsyncConsole/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using static System.Console;
 
 namespace AsyncConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://www.apple.com/");
+            
+            WriteLine("Apple's home page has {0:N0} Kilobytes.", response.Content.Headers.ContentLength/1024);
+
+            // Do not perform long-running tasks on the UI thread.
+
+            // Do not access UI elements on any thread except the UI thread.
         }
     }
 }
