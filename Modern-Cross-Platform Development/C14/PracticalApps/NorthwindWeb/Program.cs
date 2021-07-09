2ca02e6a1611a2a097030e7ac6bc50ce367b5999
Serge-N <nserge05@gmail.com>
2021-07-09 14:30:30 +0200

    mvc basics

--- Modern-Cross-Platform Development/C14/PracticalApps/NorthwindWeb/Program.cs
+++ Modern-Cross-Platform Development/C14/PracticalApps/NorthwindWeb/Program.cs
@@ -1,11 +1,12 @@
 using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
-app.UseRouting();
-app.UseHttpsRedirection();
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -16,6 +17,12 @@ else
     app.UseHsts();
 }
 
-app.MapGet("/", (Func<string>)(() => "Hello World!"));
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+app.MapGet("/hello", (Func<string>)(() => "Hello World!"));
 
 app.Run();
