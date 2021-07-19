e60999bdea2bd07c1288d772dabfc8609a353e56
Serge-N <nserge05@gmail.com>
2021-07-19 16:45:15 +0200

    Unit tesing basics-1

---
+++ Testing/Unit/Basic/BankTests/BankAccountTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using BankAccountNS;
+
+namespace BankTests
+{
+    public class BanlAccountTests
+    {
+        [Fact]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.Equal(expected, actual, 3);
+        }
+    }
+}
