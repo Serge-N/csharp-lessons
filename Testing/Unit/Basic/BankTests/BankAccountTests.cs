d0a4905cf8ef4ec018710d443eada992cf6c68dc
Serge-N <nserge05@gmail.com>
2021-07-20 22:17:40 +0200

    Rules

--- Testing/Unit/Basic/BankTests/BankAccountTests.cs
+++ Testing/Unit/Basic/BankTests/BankAccountTests.cs
@@ -14,7 +14,7 @@ namespace BankTests
             double debitAmount = 4.55;
             double expected = 7.44;
 
-            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            BankAccount account = new("Mr. Bryan Walton", beginningBalance);
 
             // Act
             account.Debit(debitAmount);
@@ -22,6 +22,38 @@ namespace BankTests
             // Assert
             double actual = account.Balance;
             Assert.Equal(expected, actual, 3);
+        }
+
+        [Fact]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new("Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [Fact]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 100.00;
+            BankAccount account = new("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // Assert
+                Assert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            }
         }
     }
 }
