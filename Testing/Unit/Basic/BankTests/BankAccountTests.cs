ca3e8a62f79f1e85c46d1015ab1a8464f2d92f5f
Serge-N <nserge05@gmail.com>
2021-07-22 23:35:53 +0200

    Thoery Testing Intro

--- Testing/Unit/Basic/BankTests/BankAccountTests.cs
+++ Testing/Unit/Basic/BankTests/BankAccountTests.cs
@@ -55,5 +55,21 @@ namespace BankTests
                 Assert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
             }
         }
+
+        // Tesing  Theories
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
     }
 }
