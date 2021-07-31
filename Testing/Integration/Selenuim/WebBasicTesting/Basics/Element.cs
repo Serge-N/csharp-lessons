322eeba39a37d12fba6fd1f1893a4e6687d4891e
Serge-N <nserge05@gmail.com>
2021-07-31 21:45:03 +0200

    IElements commands.....

---
+++ Testing/Integration/Selenuim/WebBasicTesting/Basics/Element.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBasicTesting.Basics
+{
+    public class Element
+    {
+
+        // IEments commands
+
+        // Clear
+
+        // SendKeys
+
+        // Click 
+
+        // Displayed
+
+        // Enabled 
+
+        // Selected
+
+        // Submit
+
+        // Text
+
+        // TagName
+
+        // GetCssValue
+
+        // GetAttribute
+
+        // Size
+
+        // Location
+    }
+}
