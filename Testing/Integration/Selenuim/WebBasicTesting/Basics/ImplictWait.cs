3b32adb67a85b1921d42ab2581c06d51bf167385
Serge-N <nserge05@gmail.com>
2021-08-12 08:51:29 +0200

    Wait

---
+++ Testing/Integration/Selenuim/WebBasicTesting/Basics/ImplictWait.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebBasicTesting.Basics
+{
+    public class ImplictWait
+    {
+        [Fact]
+        public void TestImplictWait()
+        {
+            IWebDriver driver = new ChromeDriver
+            {
+                Url = "https://demoqa.com/alerts"
+            };
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(500);
+
+            IWebElement element = driver.FindElement(By.Id("target"));
+        }
+
+    }
+}
