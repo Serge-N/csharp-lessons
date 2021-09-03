ccde2f4c412a9406a1c2c0a4b601056f1114b7fa
Serge-N <nserge05@gmail.com>
2021-09-03 22:14:47 +0200

    shifting to method two.

---
+++ Testing/Integration/Selenuim/ExtractionProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+Console.WriteLine("I sure miss the old class style of coding!");
+
+
+void DataAddresses()
+{ 
+
+    Console.WriteLine("");
+
+}
+
+DataAddresses();
