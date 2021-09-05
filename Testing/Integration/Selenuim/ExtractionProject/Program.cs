d76f19acc5dc66d87e76c14f6e1c0d167ca57f11
Serge-N <nserge05@gmail.com>
2021-09-05 18:50:48 +0200

    base data

--- Testing/Integration/Selenuim/ExtractionProject/Program.cs
+++ Testing/Integration/Selenuim/ExtractionProject/Program.cs
@@ -1,14 +1,22 @@
-﻿using OpenQA.Selenium;
+﻿using ExtractionProject.Data;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Text.Json;
 
 Console.WriteLine("I sure miss the old class style of coding!");
 
-
-void DataAddresses()
-{ 
-
-    Console.WriteLine("");
-
+ async Task<IEnumerable<State>> GetStatesAsync(string filename)
+{
+    string file = $"Data/{filename}.json";
+    using FileStream openStream = File.OpenRead(file);
+    return await JsonSerializer.DeserializeAsync<List<State>>(openStream);
 }
 
-DataAddresses();
+var name = "data";
+
+var g = await GetStatesAsync(name);
+
+foreach (var state in g)
+{
+    Console.WriteLine($"{state.city} : {state.zip}");
+}
