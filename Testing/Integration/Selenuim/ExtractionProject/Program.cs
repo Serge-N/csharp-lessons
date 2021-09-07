828be4d1198ff935c711b840ecede4bfefc09d42
Serge-N <nserge05@gmail.com>
2021-09-07 22:02:44 +0200

    page address

--- Testing/Integration/Selenuim/ExtractionProject/Program.cs
+++ Testing/Integration/Selenuim/ExtractionProject/Program.cs
@@ -3,20 +3,61 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Text.Json;
 
-Console.WriteLine("I sure miss the old class style of coding!");
+var name = "data";
+var TexaStates = await GetStatesAsync(name);
 
- async Task<IEnumerable<State>> GetStatesAsync(string filename)
+AcquireDataAsyn();
+
+
+async Task<IEnumerable<State>> GetStatesAsync(string filename)
 {
     string file = $"Data/{filename}.json";
     using FileStream openStream = File.OpenRead(file);
     return await JsonSerializer.DeserializeAsync<List<State>>(openStream);
 }
 
-var name = "data";
-
-var g = await GetStatesAsync(name);
-
-foreach (var state in g)
+void SaveDataAync()
 {
-    Console.WriteLine($"{state.city} : {state.zip}");
+
+}
+
+void UploadDataAsync()
+{
+
+}
+
+
+void AcquireDataAsyn()
+{
+    Console.WriteLine("Data acquisition intiated.");
+    Console.WriteLine("This is for academic purposes only.\n");
+
+    IWebDriver driver = new ChromeDriver
+    {
+        Url = "https://www.google.com/maps"
+    };
+
+    IWebElement searchBox = driver.FindElement(By.Id("searchboxinput"));
+
+    IWebElement searchButton = driver.FindElement(By.Id("searchbox-searchbutton"));
+
+    foreach (var state in TexaStates.OrderBy(x => x.country))
+    {
+        Console.WriteLine($"Extracting areas around {state.city} with zip of {state.zip}");
+
+        searchBox.SendKeys($"Apartment complex in {state.zip}");
+        searchButton.Click();
+
+        Thread.Sleep(20000);
+
+        IList<IWebElement> Results = driver.FindElements(By.PartialLinkText("https://www.google.com/maps/place/")); // needs replacement with xpath
+
+        Thread.Sleep(20000);
+
+        Console.WriteLine($"Appartments : {Results.Count()}");
+
+        Console.WriteLine("Exiting browser");
+        driver.Quit();
+        break;
+    }
 }
