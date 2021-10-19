f9a71e21825bf8e94fcc67d5c04c12ccd8825c28
Serge-N <nserge05@gmail.com>
2021-10-19 20:22:00 +0000

    Blazor transfer
    Move blazor to asp.

--- Testing/Integration/Selenuim/WebBasicTesting/Basics/TestDataAccess/UserData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebBasicTesting.Basics.TestDataAccess
-{
-    public class UserData
-    {
-        public string Key { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
